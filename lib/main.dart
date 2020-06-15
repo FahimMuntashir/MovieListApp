b08fec944a6627ee91ce964399a84d34b2f0dc1f
FahimMuntashir <fahim.tech.me@gmail.com>
2020-06-16 00:59:23 +0600

    change Ui

--- lib/main.dart
+++ lib/main.dart
@@ -42,39 +42,78 @@ class MovieListView extends StatelessWidget {
       body: ListView.builder(
         itemCount: movieList.length,
           itemBuilder: (BuildContext context, int index){
-        return Card(
-          elevation: 5.0,
-
-          color: Colors.white,
-          child: ListTile(
-            title: Text(movieList[index].title),
-            subtitle: Text("${movieList.elementAt(index).title}"),
-            leading: CircleAvatar(
-              child: Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(movieList[index].images[0]),
-                    fit: BoxFit.cover,
-                  ),
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10.0)
-                ),
-                child: null,
-              ),
-            ),
-            trailing: Text('...'),
-            onTap: (){
-              debugPrint('Movies Name : ${movieList.elementAt(index).title}');
-              Navigator.push(context, MaterialPageRoute(builder: (context){
-                return MovieDetails(movieName: movieList.elementAt(index).title,
-                movie: movieList[index],);
-              }));
-            },
-          ),
-        );
+          return movieCard(movieList[index], context);
+//        return Card(
+//          elevation: 5.0,
+//
+//          color: Colors.white,
+//          child: ListTile(
+//            title: Text(movieList[index].title),
+//            subtitle: Text("${movieList.elementAt(index).title}"),
+//            leading: CircleAvatar(
+//              child: Container(
+//                height: 200,
+//                width: 200,
+//                decoration: BoxDecoration(
+//                  image: DecorationImage(
+//                    image: NetworkImage(movieList[index].images[0]),
+//                    fit: BoxFit.cover,
+//                  ),
+//                  color: Colors.blue,
+//                  borderRadius: BorderRadius.circular(10.0)
+//                ),
+//                child: null,
+//              ),
+//            ),
+//            trailing: Text('...'),
+//            onTap: (){
+//              debugPrint('Movies Name : ${movieList.elementAt(index).title}');
+//              Navigator.push(context, MaterialPageRoute(builder: (context){
+//                return MovieDetails(movieName: movieList.elementAt(index).title,
+//                movie: movieList[index],);
+//              }));
+//            },
+//          ),
+//        );
       }),
+    );
+  }
+  Widget movieCard(Movie movie, BuildContext context){
+    return InkWell(
+      child: Container(
+        height: 120,
+        width: MediaQuery.of(context).size.width,
+        child: Card(
+          color: Colors.black12,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 8.0, left: 40.0,right: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(movie.title),
+                    Text("Country: ${movie.country}"),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Text("Released: ${movie.released}"),
+                    Text("Year: ${movie.year}"),
+//                    Text("Genre: ${movie.genre}"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      onTap: (){
+        debugPrint('I got clicked');
+      },
     );
   }
 }
